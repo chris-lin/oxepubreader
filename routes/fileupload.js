bf3da5528c03610d5468f5c69318dc00e9afd849
sean <sean@server1.ossii.com.tw>
2012-09-12 16:55:36 +0800

    - First init.

---
+++ routes/fileupload.js
@@ -0,0 +1,17 @@
+exports.home = function(req,res){
+    var i,
+        target_path;
+    var result=new Array();
+    var fs = require('fs');
+    console.log(req.files.files);
+    var tmp_path=req.files.files[0].path;
+    target_path="/data/oxenote/"+req.files.files[0].name;
+    fs.rename(tmp_path , target_path , function(err){
+    //   if (err) throw err;
+        fs.unlink(tmp_path,function(){
+            result[0]={"errorThrown":"abort"}
+            //   if(err) throw err;
+            return req;
+        });
+    });
+};

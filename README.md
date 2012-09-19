d0ba4cc05e380a4bbf2550448906bace06b5f8e9
sean <sean@server1.ossii.com.tw>
2012-09-19 17:06:55 +0800

    - Remove module mongoose. - Add README.md . - release version 0.1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# OSSII OXEpubReader
+
+OSSII OXEpubReader
+
+
+## NodeJS Modules: 
+
+* [Express](https://github.com/visionmedia/express)
+* [Connect](https://github.com/senchalabs/connect.git)
+* [Jade](https://github.com/visionmedia/jade)
+* [Unzip](https://github.com/nearinfinity/node-unzip.git)
+
+## Frontend stuffs:
+
+* [jQuery](http://jquery.com/)
+* [Twitter Bootstrap](http://twitter.github.com/bootstrap/)
+* [jQuery.resize-trigger](https://github.com/seanlin0324/jquery-plugins)
--- routes/fileupload.js
+++ routes/fileupload.js
@@ -1,42 +1,22 @@
-/*
-var mongoose = require( 'mongoose' );
-var db = require('../db.config/mongoDB.config.js');
-var file_info = db.file_info.model( 'file_info');
-var mongoose = require("mongoose");
-* */
+
 exports.index = function (req, res) {
     res.render('fileupload/index', {});
 };
 
 exports.fileupload = function (req, res) {
-    var i,
-    target_path,
-    mongo_insert,
-    upload_rs,
-    now = new Date();
+
 
     var fs = require('fs');
-    var file = req.files.files;
-    var tmp_path = file.path,
-        upload_rs = false,
-        target_path="./data/"+file.name;
+    var tmpFile = req.files.files;
+    var tmpPath = file.path
+    var targetPath = "./data/" + tmpFile.name;
     
-    fs.rename(tmp_path , target_path , function (err) {
+    fs.rename(tmpPath , targetPath , function (err) {
         if(!err){
-            var upload_rs = true;
-            /*
-            new file_info({
-                filename:file.name
-                ,filesize:file.size
-                ,filetype:file.type
-                ,upload_type:upload_rs
-                ,date:now.toLocaleTimeString()
-                }).save(function( err, user, count ){
-                    var mongo_insert=true;
-                });  
-                * */             
+            res.redirect('/unzip?entry=' + targetPath );
+        } else {
+            console.log(err);
+            res.redirect('/');
         }
-        res.redirect('/unzip?entry=' + target_path );
-        
     });
 }
